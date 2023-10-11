1565b6f1627a35d2bbc907b0a440cd6b7e8bf656
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2023-10-11 10:59:33 -0700

    Add files via upload

--- theme.js
+++ theme.js
@@ -2,13 +2,16 @@ function setTheme() {
   const savedTheme = localStorage.getItem("selectedTheme");
   const body = document.body;
   body.classList.remove("default", "light", "grain", "zip", "dawn", "black", "rose", "teal", "ice", "glacier", "hamtter", "midnight", "mastodon", "pleroma", "twitter", "x", "classic", "first");
+  
   if (savedTheme) {
-      body.classList.add(savedTheme);
+    body.classList.add(savedTheme);
+    
+    const backgroundColorHex = getComputedStyle(body).getPropertyValue("--background-color");
+    
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', backgroundColorHex);
+    console.log("hi");
   }
-
-  document.querySelector('meta[name="theme-color"]').setAttribute('content', "#000");
-  document.querySelector('meta[name="background-color"]').setAttribute('content', themeColor);
-
+  console.log("hi2");
 }
 
 window.addEventListener("load", setTheme);
