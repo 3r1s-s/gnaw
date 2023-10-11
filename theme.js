756c729db65439c224855b8588ddfd93efeb521c
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2023-10-11 11:18:10 -0700

    Add files via upload

--- theme.js
+++ theme.js
@@ -5,12 +5,8 @@ function setTheme() {
 
   if (savedTheme) {
     body.classList.add(savedTheme);
-    
-    // Access the computed value of --navbar from the linked stylesheet (styles.css)
     const computedStyle = getComputedStyle(body);
     const navbarColor = computedStyle.getPropertyValue("--navbar");
-    
-    // Set the content attribute of the meta tag to the navbar color hexcode
     document.querySelector('meta[name="theme-color"]').setAttribute('content', navbarColor);
     console.log(navbarColor);
   }
