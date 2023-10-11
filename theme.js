2095bc5eb14e4f23c98184d0a7313014fe4ff36a
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2023-10-10 21:24:58 -0700

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,7 +1,7 @@
 function setTheme() {
   const savedTheme = localStorage.getItem("selectedTheme");
   const body = document.body;
-  body.classList.remove("default", "light", "grain", "zip", "dawn", "rose", "ice", "glacier");
+  body.classList.remove("default", "light", "grain", "zip", "dawn", "black", "rose", "teal", "ice", "glacier", "hamtter", "midnight", "mastodon", "pleroma", "twitter", "x", "classic", "first");
   if (savedTheme) {
       body.classList.add(savedTheme);
   }
