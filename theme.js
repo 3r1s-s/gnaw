ef6d17006c382656e8b7ec2ee7d7f93158a91e86
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2023-10-14 22:11:56 -0700

    Add files via upload

--- theme.js
+++ theme.js
@@ -1,17 +1,30 @@
 function setTheme() {
   const savedTheme = localStorage.getItem("selectedTheme");
   const body = document.body;
-  body.classList.remove("default", "light", "grain", "zip", "dawn", "black", "rose", "teal", "ice", "glacier", "hamtter", "midnight", "mastodon", "pleroma", "twitter", "x", "classic", "first");
+  body.classList.remove(
+    "default", "light", "grain", "zip", "dawn", "black", "rose",
+    "teal", "ice", "glacier", "hamtter", "midnight", "mastodon", 
+    "pleroma", "twitter", "x", "classic", "first"
+  );
 
   if (savedTheme) {
     body.classList.add(savedTheme);
     const computedStyle = getComputedStyle(body);
-    const navbarColor = computedStyle.getPropertyValue("--navbar");
+    let navbarColor = computedStyle.getPropertyValue("--navbar");
+
+    if (!navbarColor || !isValidColor(navbarColor)) { //thanks chargpt
+      navbarColor = "#000";
+    }
+
     document.querySelector('meta[name="theme-color"]').setAttribute('content', navbarColor);
     console.log(navbarColor);
   }
 }
 
-
+function isValidColor(str) {
+  const s = new Option().style;
+  s.color = str;
+  return s.color !== '';
+}
 
 window.addEventListener("load", setTheme);
